e9e22eb68be8f0ac7204f1b2b6556521d7b2d66f
germartino <gerrymarty86@gmail.com>
2021-04-14 15:56:51 +0200

    [File-to-File] Added extracting methods feature

--- ExtractSourceFilesInfo.py
+++ ExtractSourceFilesInfo.py
@@ -6,6 +6,14 @@ class ExtractSourceFilesInfo:
     def __init__(self, repository_path):
         self._repository = RepositoryMining(repository_path)
 
+    # Append multiple value to a key in dictionary
+    def add_values_in_dict(sample_dict, key, list_of_values):
+        if not key in sample_dict:
+            sample_dict[key] = list()
+        if not list_of_values in sample_dict[key]:
+            sample_dict[key].append(list_of_values)
+        return sample_dict
+
     def getFileDevDictionary(self):
         commitDict = dict()
 
@@ -18,11 +26,34 @@ class ExtractSourceFilesInfo:
                     " and the complexity is {}".format(m.complexity)
                 )
 
-                if not m.filename in commitDict:
-                    commitDict[m.filename] = []
+                commitDict = ExtractSourceFilesInfo.add_values_in_dict(commitDict, m.filename, commit.author.name)
 
-                if not commit.author.name in commitDict[m.filename]:
-                    commitDict[m.filename].append(commit.author.name)
-                    print(commitDict)
 
         return commitDict
+
+
+    def getFileFileDictionary(self):
+        #listOfMethod = list()
+        commitDict = dict()
+        #commitLastModifiedLines = dict()
+
+        for commit in self._repository.traverse_commits():
+            for m in commit.modifications:
+                listOfMethod = list()
+                print(
+                    "\nAuthor {}".format(commit.author.name),
+                    "modified {}".format(m.filename),
+                    "source code:\n{}".format(m.source_code)
+                    # "commit last modified lines {}".format(gr.get_commits_last_modified_lines(commit))
+                )
+
+                for x in m.methods:
+                    listOfMethod.append(x.name)
+
+
+                # commitLastModifiedLines = gr.get_commits_last_modified_lines(commit)
+                #commitDict = ExtractSourceFilesInfo.add_values_in_dict(commitDict, m.filename, listOfMethod)
+                commitDict[m.filename] = listOfMethod
+
+
+        print(commitDict)
--- extraction.py
+++ extraction.py
@@ -1,7 +1,9 @@
 from ExtractSourceFilesInfo import ExtractSourceFilesInfo
 
-from_git_url = "https://github.com/vuejs-templates/webpack-simple.git"
+from_git_url = "https://github.com/biagioboi/UniShip.git"
 
 toExtract = ExtractSourceFilesInfo(from_git_url)
 
 fileDevDict = toExtract.getFileDevDictionary()
+
+toExtract.getFileFileDictionary()
